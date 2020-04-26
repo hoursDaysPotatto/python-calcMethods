9a86ca83e5dbf764cb5c3ccba54fa29d908a1965
hoursDaysPotatto <57577188+hoursDaysPotatto@users.noreply.github.com>
2020-04-27 02:49:21 +0700

    Update chebMethod.py

--- chebMethod.py
+++ chebMethod.py
@@ -1,30 +1,34 @@
-import sympy
+from sympy import Symbol
 from math import factorial
+from timeit import default_timer
 
 
 
+def chebyshev_method(f_symb,x0,n = 3,accuracy = 10**-12):
+	x = Symbol('x')
+	iterations = 0
 
-def chebyshev_method(f_symb,x0,n):
-	x = sympy.Symbol('x')
-	An = x
-	expr = x0
-	y0 = f_symb.subs(x,x0)
+	while abs(f_symb.subs(x,x0)) > accuracy:
+		iterations+=1
+		An = x
+		y0 = f_symb.subs(x,x0)
+		expr = x0
+		for i in range(n-1):
+			An   = An.diff()/f_symb.diff()
+			expr = expr + (An.subs(x,x0)*(-y0)**(i+1))/factorial(i+1)
+		x0 = expr
+	print(iterations)
+	return x0
 
-	for i in range(n-1):
-		An = An.diff()/f_symb.diff()
-		expr = expr + (An.subs(x,x0)*(-y0)**(i+1))/factorial(i+1)
+x = Symbol('x')
 
-	return expr
-y = sympy.Symbol('y')
-x = sympy.Symbol('x')
-f= x**3 - 2.1*x**2 + 1.56*x - 0.36
+f = x**3 - 2.1*x**2 + 1.56*x - 0.36
 
-inv = chebyshev_method(f,0,12)
+
+t0 = default_timer()
+inv = chebyshev_method(f,0,2)
+print("time = " + str( default_timer() - t0))
+
+print(inv)
 print(f.subs(x,inv))
-
-'''
-def chebyshev_mthd(f_symb,interval,accuracy = 10**-6):
-	x0 = (interval[0] + intervalp[1])/2
-
-	finv = find_inv(f,x0,n)
-'''
+print(inv)
